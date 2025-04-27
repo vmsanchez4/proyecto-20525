f9fa762c514aee7ce055bec2b9078478714bc1fa
vmsanchez4 <vmsanchez4@espe.edu.ec>
2025-04-27 10:16:08 -0500

    estoy segura de este commit 1

---
+++ Suma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a,b,c;
+	cout<<"Ingrese el numero a calcular"<<endl;
+	cin>>a;
+	cin>>b;
+	c=a+b;
+	cout<<"La suma es:"<< c <<endl;
+	
+	
+	
+	return 0;
+}
